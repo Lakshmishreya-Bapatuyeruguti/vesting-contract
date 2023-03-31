2b523f99bde8a212c248330c4441e814e096a51c
Lakshmishreya-Bapatuyeruguti <lakshmishreya@simformsolutions.com>
2023-03-31 08:48:24 +0530

    added events and modifiers

--- contracts/TokenVesting.sol
+++ contracts/TokenVesting.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TokenVesting{
+
     uint public duration;
     uint public cliff;
     uint public slicePeriod;
@@ -11,6 +12,7 @@ contract TokenVesting{
     mapping (address=>uint)beneficiaries;
     mapping(address => uint256) public totalTokens;
     mapping(address => uint256) public releasedTokens;
+    address owner;
     IERC20 public token;
 
     constructor(IERC20 _token,uint _cliff,uint _duration,uint _slicePeriod){
@@ -19,19 +21,44 @@ contract TokenVesting{
         cliff=_cliff;
         slicePeriod=_slicePeriod;
         startTime=block.timestamp;
+        owner=msg.sender;
 
     }
-    function lockTokens(address _beneficiary,uint tokensAmount)public {
+// Events
+    event AddBeneficiaryEvent (address beneficiary, string  message);
+    event LockTokensEvent(address beneficiary, string  message);
+    event ReleaseTokensEvent(address beneficiary, string message);
+
+// Modifiers
+    modifier cliffPeriodOver(){
+        _;
+        require(block.timestamp>=cliff,"Wait Till Cliff period");
+    }
+    modifier isBeneficiaryOrContractOwner(address _beneficiary){
+        _;
+        require(beneficiaries[_beneficiary]>0 || msg.sender==owner,"Only Beneficiary Or Owner can Release Tokens");
+    }
+    modifier onlyOwner(){
+        _;
+        require(msg.sender==owner," Only Owner can Add benefeiciaries");
+    }
+
+// Adding Beneficiaries
+    function addBeneficiary(address _beneficiary,uint tokensToLock) onlyOwner public {
+        beneficiaries[_beneficiary]=tokensToLock;
+        emit AddBeneficiaryEvent(_beneficiary, "New Beneficiary Added");
+    }
+
+// Locking Tokens
+    function lockTokens(address _beneficiary,uint tokensAmount)  public {
         beneficiaries[_beneficiary]=tokensAmount;
-        token.transfer(address(this),tokensAmount);
         totalTokens[_beneficiary]=beneficiaries[_beneficiary];
+        token.transfer(address(this),tokensAmount);
+        emit LockTokensEvent(_beneficiary, "Tokens are Locked in smart contract ");
     }
-    function releasableTokens(address _beneficiary) public view returns(uint){
-    
-        if(block.timestamp<cliff){
-        return 0;
-    }
-    else {
+
+// Calculate no. of eligible tokens to release
+    function releasableTokens(address _beneficiary) public view cliffPeriodOver returns(uint) {
         uint timeSinceStart = block.timestamp - startTime;
         uint noOfPeriodsSinceStart = timeSinceStart / slicePeriod;
         uint totalPeriods = duration / slicePeriod;
@@ -44,11 +71,14 @@ contract TokenVesting{
             return tokensToBeVested;
         }
     }
-    }
-    function releaseTokens(address _beneficiary) public { 
+
+// Release Tokens to beneficiary
+    function releaseTokens(address _beneficiary) isBeneficiaryOrContractOwner(_beneficiary) public { 
        uint tokensClaimed=releasableTokens(_beneficiary);
-       token.transfer(_beneficiary,tokensClaimed);
+       require(tokensClaimed<=beneficiaries[_beneficiary],"Tokens Claiming are greater than tokens locked");
        releasedTokens[_beneficiary]=tokensClaimed;
+       token.transfer(_beneficiary,tokensClaimed);
+       emit ReleaseTokensEvent(_beneficiary, "Tokens are Released in account of beneficiary ");
     }
 
 }
